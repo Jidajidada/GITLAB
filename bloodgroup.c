a58766eb69f51c21a63c4d3b9d5d8e338cb2f952
Jidajidada <aom.jidapa@gmail.com>
2022-09-22 02:57:16 +0700

    add second modified bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 void main()
-{ char Name[70], G[5], answer[10];
+{ char Name[99], G[5], answer[10];
   int i;
   do
   {
-    printf("What is your Name??? ");
+    printf("What is your Name?????????????? ");
     scanf("%s", Name);
     do
     {
